48dfa335e431529a06399693d75cd44ca4cdcb38
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2025-11-23 10:38:15 +0000

    make larger videos

--- ripples_global.py
+++ ripples_global.py
@@ -402,7 +402,7 @@ if __name__ == "__main__":
 
     # Set maximum sizes ...
     # NOTE: By inspection, the PNG frames are 3,840 px wide.
-    maxSizes = [512, 1024, 2048]                                                # [px]
+    maxSizes = [256, 512, 1024, 2048]                                           # [px]
 
     # Loop over maximum sizes ...
     for maxSize in maxSizes:
